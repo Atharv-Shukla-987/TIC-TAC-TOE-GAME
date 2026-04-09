4588d4c8af80de1f25f330e13c3df99e59c849db
Atharv-Shukla-987 <249597135+Atharv-Shukla-987@users.noreply.github.com>
2026-04-10 02:03:08 +0530

    done all the styling with css now going to build the logic

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("Welcome to TIC TAC TOE GAME");
+letmusic = new Audio("music.mp3");
+let audioturn = new Audio("ting.mp3");
+let gameover = new Audio("gameover.mp3");
+let turn = "X";
+let isgameover = false;
+
+// chnaging the turn 
+changeTurn = () => {
+    return turn === "X" ? "0" : "X";
+}
+//checking for a winner
+const checkWin = () => {
+
+
+}
+
+// logic
+
