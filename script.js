796669ce0826462cb4c0ff158ed65df3a45127f7
Atharv-Shukla-987 <249597135+Atharv-Shukla-987@users.noreply.github.com>
2026-04-10 05:31:01 +0530

    basics are done

--- script.js
+++ script.js
@@ -11,9 +11,54 @@ changeTurn = () => {
 }
 //checking for a winner
 const checkWin = () => {
-
+let boxtexts = document.getElementsByClassName("boxtext ")
+let wins=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+]
+wins.forEach(e =>{
+    if(boxtexts[e[0]].innerText === boxtexts[e[1]].innerText && boxtexts[e[2]].innerText === boxtexts[e[1]].innerText && boxtexts[e[0]].innerText !== ''){
+        document.querySelector('.info').innerText = boxtexts[e[0]].innerText + " WON";
+        isgameover=true;
+         document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
+    }
+})
 
 }
 
 // logic
+
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(element => {
+    let boxtext = element.querySelector('.boxtext');
+    element.addEventListener('click', (e) => {
+        if (boxtext.innerText === '') {
+            boxtext.innerText = turn;
+             turn=changeTurn();
+             audioturn.play();
+             checkWin();
+             if (!isgameover) {
+            document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+        }} 
+    }) 
+    
+
+}); 
+
+//adding on click listener to reset buttun
+reset.addEventListener('click',()=>{
+    let boxtexts = document.querySelectorAll('.boxtext');
+    Array.from(boxtexts).forEach(element =>{
+        element.innerText = "";
+    }); 
+     turn = "X";
+     document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px";
+}) 
+
 
