b603625156fbd360c9ae611c2402917fd92259b2
Atharv-Shukla-987 <249597135+Atharv-Shukla-987@users.noreply.github.com>
2026-04-12 01:25:50 +0530

    added songs
    added back ground sound  and game over song

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 console.log("Welcome to TIC TAC TOE GAME");
+let music = new Audio("music.mp3");
+let audioturn = new Audio("ting.mp3");
+let gameover = new Audio("tunak-tunak-tun.mp3");
+let gom = document.getElementById('gom')
+let bgs = document.getElementById('bgs')
 let turn = "X";
 let isgameover = false;
 
+if(!isgameover){
+    bgs.play()
+}
 // chnaging the turn 
 changeTurn = () => {
     return turn === "X" ? "0" : "X";
 }
-//checking for a winner
+//checking for a winner 
 const checkWin = () => {
 let boxtexts = document.getElementsByClassName("boxtext ")
 let wins=[
@@ -24,17 +32,22 @@ wins.forEach(e =>{
         document.querySelector('.info').innerText = boxtexts[e[0]].innerText + " WON";
         isgameover=true;
          document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
+    if(isgameover){
+        bgs.pause()
+        gom.play()
     }
+         }
 })
-
 }
 
 // logic
 
 let boxes = document.getElementsByClassName("box");
 Array.from(boxes).forEach(element => {
+    bgs.play()
     let boxtext = element.querySelector('.boxtext');
     element.addEventListener('click', (e) => {
+        bgs.play()
         if (boxtext.innerText === '') {
             boxtext.innerText = turn;
              turn=changeTurn();
@@ -50,6 +63,7 @@ Array.from(boxes).forEach(element => {
 
 //adding on click listener to reset buttun
 reset.addEventListener('click',()=>{
+    bgs.play()
     let boxtexts = document.querySelectorAll('.boxtext');
     Array.from(boxtexts).forEach(element =>{
         element.innerText = "";
